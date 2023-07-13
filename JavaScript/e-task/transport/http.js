b525449082fd9b9bb7eede7c518c26290e75f6c8
Pavel Karyapkin <karyapkin@gmail.com>
2023-07-13 16:09:42 +0600

    change http transport

--- JavaScript/e-task/transport/http.js
+++ JavaScript/e-task/transport/http.js
@@ -2,6 +2,16 @@
 
 const http = require('node:http');
 
+const HEADERS = {
+  'X-XSS-Protection': '1; mode=block',
+  'X-Content-Type-Options': 'nosniff',
+  'Strict-Transport-Security': 'max-age=31536000; includeSubdomains; preload',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Content-Type': 'application/json; charset=UTF-8',
+};
+
 const receiveArgs = async (req) => {
   const buffers = [];
   for await (const chunk of req) buffers.push(chunk);
@@ -9,14 +19,13 @@ const receiveArgs = async (req) => {
   return JSON.parse(data);
 };
 
-module.exports = (routing, port) => {
+module.exports = (routing, port, console) => {
   http.createServer(async (req, res) => {
+    res.writeHead(200, HEADERS);
+    if (req.method !== 'POST') return void res.end('"Not found"');
     const { url, socket } = req;
     const [name, method, id] = url.substring(1).split('/');
     const entity = routing[name];
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     if (!entity) return res.end('Not found');
     const handler = entity[method];
     if (!handler) return res.end('Not found');
