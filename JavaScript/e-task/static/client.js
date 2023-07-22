11ee04f916a4ca3abde3458ff06517db067b1925
Pavel Karyapkin <karyapkin@gmail.com>
2023-07-22 11:30:24 +0600

    change client scaffold for api

--- JavaScript/e-task/static/client.js
+++ JavaScript/e-task/static/client.js
@@ -1,53 +1,10 @@
 'use strict';
 
-const transport = 'http';
 const host = '127.0.0.1';
 const apiPort = '8001';
-const path = `${transport}://${host}:${apiPort}`;
-
-let socket = null;
-
-const scaffold = (transport, path, structure) => {
-  const api = {};
-  const services = Object.keys(structure);
-  for (const serviceName of services) {
-    api[serviceName] = {};
-    const service = structure[serviceName];
-    const methods = Object.keys(service);
-    for (const methodName of methods) {
-      if (transport === 'ws') {
-        socket = new WebSocket(path);
-        api[serviceName][methodName] = (...args) => new Promise((resolve) => {
-          const packet = { name: serviceName, method: methodName, args };
-          socket.send(JSON.stringify(packet));
-          socket.onmessage = (event) => {
-            const data = JSON.parse(event.data);
-            resolve(data);
-          };
-        });
-      } else if (transport === 'http') {
-        api[serviceName][methodName] = (...args) => new Promise((resolve, reject) => {
-          const url = `${path}/${serviceName}/${methodName}/${args}`;
-          fetch(url, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(args),
-          }).then(res => {
-            const { status } = res;
-            if (status !== 200) {
-              reject(new Error(`Status code: ${status}`));
-              return;
-            }
-            resolve(res.json());
-          });
-        })
-      }
-    }
-  }
-  return api;
-};
-
-const api = scaffold(transport, path, {
+const path = `http://${host}:${apiPort}`;
+const transport = {};
+const apiStructure = {
   user: {
     create: ['record'],
     read: ['id'],
@@ -60,15 +17,68 @@ const api = scaffold(transport, path, {
     delete: ['id'],
     find: ['mask'],
   },
-});
+};
 
-if (socket) {
-  socket.addEventListener('open', async () => {
-    const data = await api.user.read(3);
-    console.dir(...data);
+transport.http = url => structure => {
+  const api = {};
+  const services = Object.keys(structure);
+  for (const serviceName of services) {
+    api[serviceName] = {};
+    const service = structure[serviceName];
+    const methods = Object.keys(service);
+    for (const methodName of methods) {
+      api[serviceName][methodName] = (...args) => new Promise((resolve, reject) => {
+        const path = `${url}/${serviceName}/${methodName}/${args}`;
+        fetch(path, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(args),
+        }).then(res => {
+          const { status } = res;
+          if (status !== 200) {
+            reject(new Error(`Status code: ${status}`));
+            return;
+          }
+          resolve(res.json());
+        });
+      });
+    }
+  }
+  return Promise.resolve(api);
+};
+
+transport.ws = url => structure => {
+  const socket = new WebSocket(url);
+  const api = {};
+  const services = Object.keys(structure);
+  for (const serviceName of services) {
+    api[serviceName] = {};
+    const service = structure[serviceName];
+    const methods = Object.keys(service);
+    for (const methodName of methods) {
+      api[serviceName][methodName] = (...args) => new Promise((resolve) => {
+        const bunch = { name: serviceName, method: methodName, args };
+        socket.send(JSON.stringify(bunch));
+        socket.onmessage = (event) => {
+          const data = JSON.parse(event.data);
+          resolve(data);
+        };
+      });
+    }
+  }
+  return new Promise((resolve) => {
+    socket.addEventListener('open', () => resolve(api));
   });
-} else {
-  api.user.read(3).then(data => {
-    console.dir(...data);
-  });
-}
+};
+
+const scaffold = url => {
+  const protocol = url.startsWith('ws:') ? 'ws' : 'http';
+  return transport[protocol](url);
+};
+
+(async () => {
+  const api = await scaffold(path)(apiStructure);
+  console.log(api);
+  const data = await api.user.read(3);
+  console.dir(...data);
+})();
